913e158e605ff0949e3c05cd5d31f179c1b370d6
Kræn Hansen <kraen.hansen@mongodb.com>
2022-11-03 10:19:59 +0100

    Renamed to IndirectWeakCache

--- packages/realm/src/internal.ts
+++ packages/realm/src/internal.ts
@@ -42,7 +42,7 @@ export * from "./TimeoutPromise";
 /** @internal */
 export * from "./IndirectWeakMap";
 /** @internal */
-export * from "./WeakCache";
+export * from "./IndirectWeakCache";
 
 /** @internal */
 export * from "./PropertyHelpers";
